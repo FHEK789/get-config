fc534ee89bc8b5ce4c667bd1fe9c4f2f676e1dac
Felipe Kinoshita <kinofhek@gmail.com>
2020-11-28 21:46:38 -0300

    feat: add config for alacritty and starship

--- init.sh
+++ init.sh
@@ -2,3 +2,11 @@
 
 echo "--- GETTING CONFIG ---"
 
+# alacritty configuration
+mkdir -p $HOME/.config/alacritty
+
+cp ./alacritty/alacritty.yml $HOME/.config/alacritty/
+cp ./alacritty/dracula.yml $HOME/.config/alacritty/
+
+# starship configuration
+cp ./starship/starship.toml $HOME/.config/
