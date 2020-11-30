5df1a58add1120f348a0bfa4a11f694284df0b50
Felipe Kinoshita <kinofhek@gmail.com>
2020-11-30 06:21:09 -0300

    feat: add vscode configurations

--- init.sh
+++ init.sh
@@ -11,6 +11,10 @@ cp ./alacritty/dracula.yml $HOME/.config/alacritty/
 # starship configuration
 cp ./starship/starship.toml $HOME/.config/
 
+# vscode configuration
+mkdir -p $HOME/.config/Code/User
+cp ./vscode/settings.json $HOME/.config/Code/User/settings.json
+
 sleep 1
 
 echo "restart alacritty to apply configurations"
