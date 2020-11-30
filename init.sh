46eadcdc756684a0f22d316a6be71aeefe43a6bc
Felipe Kinoshita <kinofhek@gmail.com>
2020-11-30 16:46:04 -0300

    feat: add shell configurations

--- init.sh
+++ init.sh
@@ -15,6 +15,9 @@ cp ./starship/starship.toml $HOME/.config/
 mkdir -p $HOME/.config/Code/User
 cp ./vscode/settings.json $HOME/.config/Code/User/settings.json
 
+# shell configurations
+cp ./shell/fhek789.zsh $HOME/.oh-my-zsh/custom/fhek789.zsh
+
 sleep 1
 
 echo "restart alacritty to apply configurations"
---
+++ shell/fhek789.zsh
@@ -0,0 +1,8 @@
+eval "$(starship init zsh)"
+
+export EDITOR="/usr/bin/nvim"
+
+alias sudo="sudo "
+alias v="nvim"
+alias zc="nvim $HOME/.zshrc"
+alias za="source $HOME/.zshrc"
