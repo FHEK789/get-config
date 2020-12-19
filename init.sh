7e1c05ac02622af7ec3f7a038d0f53ecb07ae403
Felipe Kinoshita <kinofhek@gmail.com>
2020-12-19 00:42:30 -0300

    feat: test if directory exists before copying files

--- init.sh
+++ init.sh
@@ -9,15 +9,28 @@ echo "--- GETTING CONFIG ---"
 cp ./starship/starship.toml $HOME/.config/
 
 # vscode configuration
-mkdir -p $HOME/.config/Code/User
-cp ./vscode/settings.json $HOME/.config/Code/User/settings.json
+if [[ -d "$HOME/.config/Code/User" ]]; then
+	cp ./vscode/settings.json $HOME/.config/Code/User/settings.json
+else
+	mkdir -p $HOME/.config/Code/User
+	cp ./vscode/settings.json $HOME/.config/Code/User/settings.json
+fi
 
 # shell configurations
-cp ./shell/fhek789.zsh $HOME/.oh-my-zsh/custom/fhek789.zsh
 cp ./shell/zshrc $HOME/.zshrc
 
+if [[ -d "$HOME/.oh-my-zsh/custom"]]; then
+	cp ./shell/fhek789.zsh $HOME/.oh-my-zsh/custom/fhek789.zsh
+else
+	echo "Install oh-my-zsh beforehand."
+fi
+
 # csgo binds
-cp ./csgo/binds.cfg $HOME/.steam/steam/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/cfg/
+if [[ -d "$HOME/.steam/steam/steampps/common/Counter-Strick\ Global\ Offensive/csgo/cfg"]]; then
+	cp ./csgo/binds.cfg $HOME/.steam/steam/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/cfg/
+else
+	echo "Unable to copy cfg to CSGO folder."
+fi
 
 sleep 1
 
